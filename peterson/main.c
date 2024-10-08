339f085cea6e7adbc247a4a67c02a6087c0a8b7e
Alexander Andreev <andreev@h140.net58.bmstu.ru>
2024-10-08 18:20:20 +0300

    upadte with diff num of proc

--- peterson/main.c
+++ peterson/main.c
@@ -5,31 +5,43 @@
 #include <time.h>
 
 #define NUM_THREADS 2 // Число потоков
-#define NUM_ITERATIONS 10000000 // Число итераций для каждого потока
+#define NUM_ITERATIONS 100000000 // Число итераций для каждого потока
 #define WAIT_TIME 1 // Время ожидания в секундах
 
 int want[NUM_THREADS] = {0, 0}; // Массив флагов для потоков
 int turn; // Переменная, указывающая чей сейчас ход
 volatile int conflict_counter = 0; // Счетчик конфликтов
+volatile int total_counter = 0;
+
+static int lim[ 16 ] = { 1, 13, 12, 9, 7, 15, 4, 3, 2,  11, 14, 5, 6, 8, 10, 0 };
 
 void *thread_function(void *arg) {
     int id = *(int *)arg;
+    int n = 1231;
 
     for (int i = 0; i < NUM_ITERATIONS; ++i) {
+
+        for (volatile int j = 10*lim[n & 15] + 1; j >= 0; --j) {
+        }
+
         want[id] = 1; // Я хочу войти
         turn = 1 - id; // Теперь очередь другого потока
         while (want[1 - id] && turn == 1 - id); // Ждем, пока другой поток не закончится
-
+        n = __atomic_fetch_add( &total_counter, 1, __ATOMIC_SEQ_CST );
         // Критическая секция
-        printf("Поток %d вошёл в критическую секцию\n", id);
-        sleep(WAIT_TIME); // Имитация работы в критической секции
-        printf("Поток %d покинул критическую секцию\n", id);
+//        printf("Поток %d вошёл в критическую секцию\n", id);
+//        sleep(WAIT_TIME); // Имитация работы в критической секции
+//        printf("Поток %d покинул критическую секцию\n", id);
+
+        for (volatile int j = 10*lim[n & 15] + 1; j >= 0; --j) {
+        }
 
         // Проверка на конфликты
-        if (want[1 - id]) {
-            conflict_counter++;
-            printf("Конфликт обнаружен между потоками!\n");
+        if ( 1 == ( 1 & n ) ) {
+            __atomic_fetch_add( &conflict_counter, 1, __ATOMIC_SEQ_CST );
+//            printf("Конфликт обнаружен между потоками!\n");
         }
+        __atomic_fetch_add( &total_counter, 1, __ATOMIC_SEQ_CST );
 
         // Завершение ввода
         want[id] = 0; // Я вышел

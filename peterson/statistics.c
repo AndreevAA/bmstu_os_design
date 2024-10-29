5853fbc71bb2240c620746a2b956c249ba116c2b
Alexander Andreev <andreev@h252.net58.bmstu.ru>
2024-10-29 18:09:55 +0300

    update

--- peterson/statistics.c
+++ peterson/statistics.c
@@ -9,7 +9,7 @@
 
 #define NUM_THREADS 2 // Количество потоков
 #define WAIT_TIME 10 // Время ожидания в "операциях"
-#define MAX_TESTS 14 // Максимальное количество тестов
+#define MAX_TESTS 10 // Максимальное количество тестов
 #define MAX_ITERATIONS 2500 // Максимальное количество итераций для теста
 
 int want[NUM_THREADS] = {0, 0}; // Массив флагов для потоков
@@ -21,12 +21,17 @@ volatile int operation_cnt_id_one = 0; // Счетчик операций для
 
 static int lim[ 16 ] = { 1, 13, 12, 9, 7, 15, 4, 3, 2,  11, 14, 5, 6, 8, 10, 0 };
 
+volatile int flag1, flag2;
 
 void *thread_function(void *arg) {
+
     int id = *(int *)arg;
     int n = 1231;
 
     int iterations = *(int *)(arg + sizeof(int));
+
+    __atomic_fetch_add( &flag1, 1, __ATOMIC_SEQ_CST );
+    while (!flag2) {}
 
     for (int i = 0; i < iterations; ++i) {
         // Выполнение операций
@@ -69,7 +74,9 @@ void *thread_function(void *arg) {
 
 void run_tests_1() {
     FILE *file = fopen("results.txt", "w");
-    int iterations_arr[MAX_TESTS] = {100, 200, 500, 1000, 2000, 5000, 10000, 20000, 40000, 80000, 160000, 350000, 1000000, 5000000};
+    int iterations_arr[MAX_TESTS] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+//    int iterations_arr[MAX_TESTS] = {100, 200, 500, 1000, 2000, 5000, 10000, 20000, 40000, 80000, 160000, 350000, 1000000, 5000000};
+//    int iterations_arr[MAX_TESTS] = {1000000, 2000000, 3000000, 4000000, 5000000, 6000000, 7000000};
 
     for (int t = 0; t < MAX_TESTS; t++) {
         int iterations = iterations_arr[t];
@@ -82,9 +89,13 @@ void run_tests_1() {
         // Передаем количество итераций в аргументах потокам
         int thread_args[NUM_THREADS][2] = {{0, iterations}, {1, iterations}};
 
+        flag1 = flag2 = 0;
         for (int i = 0; i < NUM_THREADS; i++) {
             pthread_create(&threads[i], NULL, thread_function, (void*)thread_args[i]);
         }
+
+        while ( flag1 != 2 ) {}
+        flag2=1;
 
         for (int i = 0; i < NUM_THREADS; i++) {
             pthread_join(threads[i], NULL);
